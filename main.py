4210ae6fb209d6e8ac794e73c90745a69a40b9a9
lookbackjh <jjjjunho1011@gmail.com>
2024-01-04 18:10:20 +0900

    update done

--- main.py
+++ main.py
@@ -39,6 +39,8 @@ def large_categories_getter(source_idx,target_idx):
     # find row where 대분류 is largeones[0]
     largerows=a.loc[(a['대분류'] == largeones[source_idx])] 
     targets=largerows['소분류'].unique()
+
+
     targetrows=largerows.loc[(largerows['소분류'] == targets[target_idx])]
     # sum every row
     targetrows.drop(['ID','대분류','중분류','소분류','브랜드','제품'],axis=1,inplace=True) 
@@ -100,8 +102,8 @@ def main():
     
 
     ## 2. 데이터 전처리
-    seq_length=30 # 얼마의 기간을 가지고 다음 기간을 예측할 것인가
-    pred_length=5 # 다음 기간을 얼마나 예측할 것인가 ex) 다음 5일에 대한 예측치를 한번에 제공
+    seq_length=100 # 얼마의 기간을 가지고 다음 기간을 예측할 것인가
+    pred_length=50 # 다음 기간을 얼마나 예측할 것인가 ex) 다음 5일에 대한 예측치를 한번에 제공
     xs,ys=create_sequence(source_data,seq_length,pred_length)
     xt,yt=create_sequence(target_data,seq_length,pred_length)
     
@@ -109,8 +111,8 @@ def main():
     train_xs,train_ys,test_xs,test_ys=preprocessing(xs,ys) # train test split, min max scaler    
     train_xt,train_yt,test_xt,test_yt=preprocessing(xt,yt) # train test split, min max scaler
 
-    train_s_label=np.zeros(len(train_xs))
-    train_t_label=np.ones(len(train_xt))
+    train_s_label=np.ones(len(train_xs))
+    train_t_label=np.zeros(len(train_xt))
 
     discriminative_dataset=Discriminative_Dataset(train_xs,train_xt,train_s_label,train_t_label)
     discriminative_loader=DataLoader(discriminative_dataset,batch_size=32,shuffle=True)
@@ -126,7 +128,7 @@ def main():
 
     ## ADDA 1. Source 모델 훈련시키기
     source_model = Forecaster(input_dim=1, hidden_dim=64,output_dim=pred_length) #LSTM Encoder
-    source_trainer=pl.Trainer(max_epochs=1)
+    source_trainer=pl.Trainer(max_epochs=200)
     source_trainer.fit(source_model,train_loader_source,test_loader_sourcer)
 
 
@@ -134,38 +136,39 @@ def main():
     ## ADDA 2. Discriminator 훈련, Target Encoder 훈련 (번갈아 가면서)
     target_encoder=Encoder(input_dim=1, hidden_dim=64)
     discriminator=Discriminator(source_model.encoder,target_encoder, hidden_dim=64, latent_dim=64)
-    discriminator_trainer=pl.Trainer(max_epochs=1)
+    discriminator_trainer=pl.Trainer(max_epochs=200)
     discriminator_trainer.fit(discriminator,discriminative_loader)
 
     ## ADDA 3. Evaluation
     # 1) Source 모델 평가
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
     source_model.eval()
-    
-    test_xt=torch.tensor(test_xt,dtype=torch.float32).to(source_model.device)
+    source_model=source_model.to(device)
+
+    test_xt=torch.tensor(test_xt,dtype=torch.float32).to(device)
     #test_xt=test_xt.unsqueeze(2)
     source_output=source_model.forward(test_xt)
     source_output=source_output.squeeze()
-    source_output=source_output.detach().numpy()
-    source_output=source_output.reshape(-1,1)
+    source_output=source_output.detach().cpu().numpy()
     # evaluate rmse
     from sklearn.metrics import mean_squared_error
     from math import sqrt
-    rmse = sqrt(mean_squared_error(test_yt, source_output))
+    rmse = sqrt(mean_squared_error(test_yt[:,49], source_output[:,49]))
     print('Test RMSE using source only: %.3f' % rmse)
     
     
     # 2) Target 모델 평가
     target_encoder.eval()
+    target_encoder=target_encoder.to(device)
     target_output=target_encoder.forward(test_xt)
     tar_pred=source_model.forecaster(target_output[:,-1,:])
     tar_pred=tar_pred.squeeze()
-    tar_pred=tar_pred.detach().numpy()
-    tar_pred=tar_pred.reshape(-1,1)
+    tar_pred=tar_pred.detach().cpu().numpy()
     # evaluate rmse
     from sklearn.metrics import mean_squared_error
     from math import sqrt
-    rmse = sqrt(mean_squared_error(test_yt, tar_pred))
-    print('Test RMSE using target only: %.3f' % rmse)
+    rmse = sqrt(mean_squared_error(test_yt[:,49], tar_pred[:,49]))
+    print('Test RMSE using target encoder trained: %.3f' % rmse)
 
 
 
